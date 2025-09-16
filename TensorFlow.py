1c5740cbe740d2873fb61cdc5d4da049ae20916e
kerekesdaniel480 <kerekesdaniel480@gmail.com>
2025-09-16 21:46:13 +0200

    -c

--- TensorFlow.py
+++ TensorFlow.py
@@ -1,3 +1,24 @@
 import tensorflow as tf
+import sys
 
-print("TensorFlow version: ", tf.__version__)
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = datagen.flow_from_directory(
+    'Dataset_cats/Cat vs Dog/train',
+    target_size=(300, 300),
+    batch_size=32,
+    class_mode='binary'
+)
+
+validation_generator = datagen.flow_from_directory(
+    'Dataset_cats/Cat vs Dog/validation',
+    target_size=(300, 300),
+    batch_size=32,
+    class_mode='binary'
+)
+
+
+print("TensorFlow version: ", tf.__version__, "\n")
+print("Python version: ", sys.version, "\n")
