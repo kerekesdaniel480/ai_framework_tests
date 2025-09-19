5f15464f009b5e1f0c30e84216ad77ac0c953139
kerekesdaniel480 <kerekesdaniel480@gmail.com>
2025-09-19 22:36:41 +0200

    c

--- TensorFlow.py
+++ TensorFlow.py
@@ -22,5 +22,5 @@ validation_generator = datagen.flow_from_directory(
 )
 
 
-print("TensorFlow version: ", tf.__version__, "\n")
-print("Python version: ", sys.version, "\n")
+#print("TensorFlow version: ", tf.__version__, "\n")
+#print("Python version: ", sys.version, "\n")
