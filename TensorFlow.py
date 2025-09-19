775fecf23ab8de5f416e6b7ba259690ccf76391c
kerekesdaniel480 <kerekesdaniel480@gmail.com>
2025-09-19 22:36:14 +0200

    c

--- TensorFlow.py
+++ TensorFlow.py
@@ -2,6 +2,8 @@ import tensorflow as tf
 import sys
 
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 
 datagen = ImageDataGenerator(rescale=1./255)
 
