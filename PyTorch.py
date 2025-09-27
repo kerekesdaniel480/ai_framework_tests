a9f9657bf7e0df88337812adb1c3be150be881dd
kerekesdaniel480 <kerekesdaniel480@gmail.com>
2025-09-27 17:37:47 +0200

    c

--- PyTorch.py
+++ PyTorch.py
@@ -6,6 +6,14 @@ from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 import os
 
+data_dir = '/Users/kerekesdaniel/Projects/AI_Test/ai_framework_tests/Dataset_cats/Cat_vs_Dog/train'
+
+transform = transforms.Compose([
+    transforms.Resize((300, 300)),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+])
 
 print("PyTorch version:", torch.__version__, "\n")
 
