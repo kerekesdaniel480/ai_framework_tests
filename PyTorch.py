0791c2b15850622625c73afce57c5806e9580acf
kerekesdaniel480 <kerekesdaniel480@gmail.com>
2025-09-27 17:35:13 +0200

    c

--- PyTorch.py
+++ PyTorch.py
@@ -1,4 +1,11 @@
+
 import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+import os
+
 
 print("PyTorch version:", torch.__version__, "\n")
 
