5129edbf0d0f206fb7b56f21dc3cfae6ea415886
kerekesdaniel480 <kerekesdaniel480@gmail.com>
2025-09-27 16:28:15 +0200

    c

--- TensorFlow.py
+++ TensorFlow.py
@@ -17,10 +17,10 @@ lr_scheduler = ReduceLROnPlateau(
 
 train_datagen = ImageDataGenerator(rescale=1./255,
                              rotation_range=20,
-                             width_shift_range=0.2,
-                             height_shift_range=0.2,
-                             shear_range=0.2,
-                             zoom_range=0.2,
+                             width_shift_range=0.1,
+                             height_shift_range=0.1,
+                             shear_range=0.1,
+                             zoom_range=0.1,
                              horizontal_flip=True,
                              fill_mode='nearest')
 
