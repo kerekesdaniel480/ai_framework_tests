7eb2386c6bb835ea7c5f8a9a0788ddc4b6ae66cb
kerekesdaniel480 <kerekesdaniel480@gmail.com>
2025-09-28 18:42:13 +0200

    c

--- PyTorch.py
+++ PyTorch.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
-#import os
+import os
 
 data_dir = '/Users/kerekesdaniel/Projects/AI_Test/ai_framework_tests/Dataset_cats/Cat_vs_Dog/train'
 
@@ -15,7 +15,7 @@ transform = transforms.Compose([
     transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
 ])
 
-print("PyTorch version:", torch.__version__, "\n")
+print("PyTorch version: ", torch.__version__, "\n")
 
 import torch
 x = torch.rand(5, 3)
