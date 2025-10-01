7263d166189847ab731d78dfe1b26f37cd8eb10e
kerekesdaniel480 <kerekesdaniel480@gmail.com>
2025-10-01 23:49:52 +0200

    c

--- TensorFlow_Numbers.py
+++ TensorFlow_Numbers.py
@@ -5,4 +5,4 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 from tensorflow.keras.callbacks import EarlyStopping
-#from tensorflow.keras.callbacks import ReduceLROnPlateau
+from tensorflow.keras.callbacks import ReduceLROnPlateau
