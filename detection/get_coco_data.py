26b236261ddff1157d13f33f79d9c9e96bd1ac41
kerekesdaniel480 <kerekesdaniel480@gmail.com>
2025-12-28 16:04:41 +0100

    commit
    Former-commit-id: bce26c278f6eb54dbf2a96fbaf2daeb36d4c850d

--- detection/get_coco_data.py
+++
@@ -1,4 +0,0 @@
-from pycocotools.coco import COCO
-c = COCO('/Users/kerekesdaniel/Projects/AI_Test/ai_framework_tests/detection/coco/annotations/instances_train2017.json')
-cats = c.loadCats(c.getCatIds('car'))
-print(cats)
